758a9b839493400247888257cf6f3801fdc00ea2
Marco Antonio Rodriguez Guzman <marco.rodriguez@clickittech.com.mx>
2023-02-07 01:54:28 -0600

    jenkinsfile

--- env/demo/locals.tf
+++ env/demo/locals.tf
@@ -8,6 +8,7 @@ locals {
   cd $workdir
   sudo yum update -y
   sudo amazon-linux-extras install java-openjdk11 -y
+  sudo curl -Lo /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
   sudo tee /etc/yum.repos.d/jenkins.repo<<EOF
   [jenkins]
   name=Jenkins
