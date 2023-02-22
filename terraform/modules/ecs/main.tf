a89fa74d768891844a7d32e3a2e8240e71487024
Marco Antonio Rodriguez Guzman <marco.rodriguez@clickittech.com.mx>
2023-02-21 21:00:35 -0600

    [chore] final changes

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -74,11 +74,48 @@ resource "aws_ecs_service" "svc" {
   }
 }
 
+resource "aws_iam_instance_profile" "ecs" {
+  name  = "ecs-${var.name}-${data.aws_region.current.name}"
+  role  = aws_iam_role.ecs.name
+}
+
+resource "aws_iam_role" "ecs" {
+  name  = "ecs-${var.name}-${data.aws_region.current.name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_ssm" {
+  role       = aws_iam_role.ecs.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_ecs" {
+  role       = aws_iam_role.ecs.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
 resource "aws_launch_template" "lt" {
   name_prefix = "${var.name}-tmplt"
   image_id = data.aws_ami.ecs_ami.id
   instance_type = "t2.micro"
-
+  iam_instance_profile {
+    name = resource.aws_iam_instance_profile.ecs.name
+  }
   # User data script to run on instances at launch
   user_data =base64encode(var.user_data)
 
