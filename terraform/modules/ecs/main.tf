5ce780be11bccaae3ab276a064ec4a9104f53f98
Marco Antonio Rodriguez Guzman <marco.rodriguez@clickittech.com.mx>
2023-02-21 20:16:27 -0600

    [chore] final changes

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -108,7 +108,7 @@ resource "aws_autoscaling_group" "asg" {
     id = aws_launch_template.lt.id
     version = "$Latest"
   }
-  enable_deletion_protection = false
+  protect_from_scale_in = false
   availability_zones = var.availability_zones
   max_size = 1
   min_size = 0
