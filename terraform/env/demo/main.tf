02bc364207f325ef7dd731faeddc637ec59d2d21
Marco Antonio Rodriguez Guzman <marco.rodriguez@clickittech.com.mx>
2023-02-21 19:29:52 -0600

    Update main.tf

--- terraform/env/demo/main.tf
+++ terraform/env/demo/main.tf
@@ -105,14 +105,24 @@ module "alb" {
 module "ecs-cluster" {
   source = "terraform-aws-modules/ecs/aws"
   cluster_name = local.name
-  # cluster_configuration = {
-  #   execute_command_configuration = {
-  #     logging = "OVERRIDE"
-  #     log_configuration = {
-  #       cloud_watch_log_group_name = "/aws/ecs/cluster/${local.name}"
-  #     }
-  #   }
-  # }
+   
+  autoscaling_capacity_providers = {
+    one = {
+      auto_scaling_group_arn         = module.ecs-service.asg.arn
+      managed_termination_protection = "ENABLED"
+
+      managed_scaling = {
+        maximum_scaling_step_size = 3
+        minimum_scaling_step_size = 1
+        status                    = "ENABLED"
+        target_capacity           = 60
+      }
+
+      default_capacity_provider_strategy = {
+        weight = 60
+        base   = 20
+      }
+    }
 }
 
 ###########################################
